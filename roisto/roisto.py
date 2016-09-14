99f6700e652a8007db642ae252bb452c0080a40f
haphut <haphut@gmail.com>
2016-09-14 12:59:15 +0300

    Add main and utils

---
+++ roisto/roisto.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Main module."""
+
+import asyncio
+import logging
+import logging.config
+import threading
+
+from roisto import cmdline
+from roisto import mqttpublisher
+from roisto import predictionpoller
+from roisto import util
+
+
+def main():
+    """Main function."""
+    args = cmdline.parse_cmdline()
+    config = args.config
+
+    logging.config.dictConfig(config['logging'])
+    logger = logging.getLogger(__name__)
+    logger.info('roisto started.')
+
+    loop = asyncio.get_event_loop()
+    async_helper = util.AsyncHelper(loop, executor=None)
+
+    queue = asyncio.Queue()
+    is_mqtt_connected = threading.Event()
+    mqtt_publisher = mqttpublisher.MQTTPublisher(config['mqtt'], async_helper,
+                                                 queue, is_mqtt_connected)
+
+    prediction_poller = predictionpoller.PredictionPoller(
+        config['pubtrans'], async_helper, queue, is_mqtt_connected)
+
+    tasks = [
+        mqtt_publisher.run(),
+        prediction_poller.run(),
+    ]
+    loop.run_until_complete(async_helper.wait_for_first(tasks))
+    logger.error('Either mqtt_publisher or prediction_poller completed.')
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ roisto/util.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Utilities."""
+
+import asyncio
+import concurrent.futures
+
+import isodate
+
+
+def convert_duration_to_seconds(duration):
+    """Convert ISO 8601 duration to float seconds."""
+    return isodate.parse_duration(duration).total_seconds()
+
+
+class AsyncHelper:
+    """Collect asyncio helpers that require the same loop and executor."""
+
+    def __init__(self, loop, executor=None):
+        self.loop = loop
+        self.executor = executor
+
+    async def ensure_future(self, coro_or_future, *args):
+        """Create a task."""
+        return await asyncio.ensure_future(
+            coro_or_future, *args, loop=self.loop)
+
+    async def run_in_executor(self, func, *args):
+        """Use asyncio.run_in_executor() easily."""
+        return await self.loop.run_in_executor(self.executor, func, *args)
+
+    async def wait_for_first(self, futures, *args):
+        """Wait until the first future finishes."""
+        return await asyncio.wait(
+            futures,
+            *args,
+            loop=self.loop,
+            timeout=None,
+            return_when=concurrent.futures.FIRST_COMPLETED)
+
+    async def wait_forever(self, future):
+        """Wait for a future until it is done."""
+        return await asyncio.wait_for(future, timeout=None, loop=self.loop)
+
+    async def wait_for_event(self, event, *args):
+        """Use threading.Event.wait() with asyncio."""
+        return await self.run_in_executor(event.wait, *args)
+
+    async def sleep(self, *args, **kwargs):
+        """Sleep in the right loop."""
+        return await asyncio.sleep(*args, **kwargs, loop=self.loop)
