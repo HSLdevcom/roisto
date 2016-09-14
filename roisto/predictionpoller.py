91238f34847a6f50032cbcbd38da951733642b68
haphut <haphut@gmail.com>
2016-09-14 15:04:17 +0300

    First version that somehow works

--- roisto/predictionpoller.py
+++ roisto/predictionpoller.py
@@ -25,8 +25,11 @@ def _minutes_to_hours_string(minutes):
 
 
 def _combine_into_timestamp(naive_datetime, utc_offset_minutes):
-    return (naive_datetime.isoformat() +
-            _minutes_to_hours_string(utc_offset_minutes))
+    naive_string = naive_datetime.isoformat()
+    if '.' in naive_string:
+        # Do not show more than milliseconds.
+        naive_string = naive_string[:-3]
+    return naive_string + _minutes_to_hours_string(utc_offset_minutes)
 
 
 def _connect_and_query(connect, query):
@@ -61,26 +64,30 @@ class PredictionPoller:
         self._connect = functools.partial(
             pymssql.connect,
             server=config['host'],
-            user=config['user'],
+            user=config['username'],
             password=config['password'],
             port=config['port'])
         self._doi_connect = functools.partial(
-            self._connect, database=config['doi'])
+            self._connect, database=config['doi_database'])
         self._roi_connect = functools.partial(
-            self._connect, database=config['roi'])
+            self._connect, database=config['roi_database'])
 
         # Dictionaries for matching PubTrans IDs to Jore IDs.
-        self._stops = {}
-        self._lines = {}
+        self._stops = None
+        self._journeys = None
 
         # Accounting for the timezone.
-        self._utc_offsets = {}
+        self._utc_offsets = None
 
         # Query intervals.
         self._stop_query_interval_in_seconds = util.convert_duration_to_seconds(
             config['stop_query_interval'])
-        self._line_query_interval_in_seconds = util.convert_duration_to_seconds(
-            config['line_query_interval'])
+        self._journey_query_interval_in_seconds = util.convert_duration_to_seconds(
+            config['journey_query_interval'])
+        self._utc_offset_query_interval_in_seconds = util.convert_duration_to_seconds(
+            config['utc_offset_query_interval'])
+        self._prediction_query_interval_in_seconds = util.convert_duration_to_seconds(
+            config['prediction_query_interval'])
 
     # FIXME: Use or remove.
     ## Example usage:
@@ -97,16 +104,19 @@ class PredictionPoller:
 
     async def _keep_polling_stops(self):
         STOP_QUERY = """
-            SELECT DISTINCT
+            SELECT
                 CONVERT(CHAR(16), Gid) AS JourneyPatternPointGid,
                 CONVERT(CHAR(7), Number) AS JoreStopId
             FROM
                 JourneyPatternPoint
         """
         while True:
+            query = STOP_QUERY
+            LOG.debug('Querying stops from DOI:' + query)
             try:
                 result = await self._async_helper.run_in_executor(
-                    _connect_and_query, self._doi_connect, STOP_QUERY)
+                    _connect_and_query, self._doi_connect, query)
+                LOG.debug('Got {length} stops.'.format(length=len(result)))
                 self._stops = dict(result)
             except pymssql.Error as ex:
                 LOG.warning('SQL error: ' + str(ex))
@@ -115,10 +125,15 @@ class PredictionPoller:
             await self._async_helper.sleep(
                 self._stop_query_interval_in_seconds)
 
-    async def _keep_polling_lines(self):
-        LINE_QUERY = """
+    async def _keep_polling_journeys(self):
+        JOURNEY_QUERY = """
             SELECT
                 CONVERT(CHAR(16), DVJ.Id) AS DatedVehicleJourneyId,
+                CONCAT(
+                    CONVERT(CHAR(8), DVJ.OperatingDayDate, 112),
+                    ':',
+                    CONVERT(CHAR(16), DVJ.Gid)
+                ) AS DatedVehicleJourneyUniqueGid,
                 KVV.StringValue AS JoreLineId,
                 SUBSTRING(
                     CONVERT(CHAR(16), VJT.IsWorkedOnDirectionOfLineGid),
@@ -174,35 +189,42 @@ class PredictionPoller:
                 now, PredictionPoller._AT_LEAST_DAYS_BACK_SHIFT)
             future_utc = _timestamp_day_shift(
                 now, PredictionPoller._AT_MOST_DAYS_FORWARD_SHIFT)
-            query = LINE_QUERY.format(past_utc=past_utc, future_utc=future_utc)
+            query = JOURNEY_QUERY.format(past_utc=past_utc, future_utc=future_utc)
+            LOG.debug('Querying journeys from DOI:' + query)
             try:
                 result = await self._async_helper.run_in_executor(
                     _connect_and_query, self._doi_connect, query)
+                LOG.debug('Got {length} journeys.'.format(length=len(result)))
                 rearranged = {}
                 for row in result:
-                    rearranged[row[0]] = {
-                        'JoreLineId': row[1],
-                        'JoreDirection': row[2],
-                        'LocalizedStartTime': row[3],
+                    rearranged[row[1]] = {
+                        'JoreLineId': row[2],
+                        'JoreDirection': row[3],
+                        'LocalizedStartTime': row[4],
                     }
-                self._lines = rearranged
+                self._journeys = rearranged
             except pymssql.Error as ex:
                 LOG.warning('SQL error: ' + str(ex))
             except pymssql.Warning as ex:
                 LOG.warning('SQL warning: ' + str(ex))
             await self._async_helper.sleep(
-                self._line_query_interval_in_seconds)
+                self._journey_query_interval_in_seconds)
 
     async def _keep_polling_utc_offsets(self):
         UTC_OFFSET_QUERY = """
             SELECT
                 Id AS DatedVehicleJourneyId,
+                CONCAT(
+                    CONVERT(CHAR(8), OperatingDayDate, 112),
+                    ':',
+                    CONVERT(CHAR(16), Gid)
+                ) AS DatedVehicleJourneyUniqueGid,
                 UTCOffsetMinutes
             FROM
                 DatedVehicleJourney
             WHERE
-                DVJ.OperatingDayDate >= '${past_utc}'
-                AND DVJ.OperatingDayDate < '${future_utc}'
+                OperatingDayDate >= '{past_utc}'
+                AND OperatingDayDate < '{future_utc}'
         """
         while True:
             now = datetime.datetime.utcnow()
@@ -212,10 +234,12 @@ class PredictionPoller:
                 now, PredictionPoller._AT_MOST_DAYS_FORWARD_SHIFT)
             query = UTC_OFFSET_QUERY.format(
                 past_utc=past_utc, future_utc=future_utc)
+            LOG.debug('Querying UTC offsets from ROI:' + query)
             try:
                 result = await self._async_helper.run_in_executor(
                     _connect_and_query, self._roi_connect, query)
-                self._utc_offsets = dict(result)
+                LOG.debug('Got {length} UTC offsets.'.format(length=len(result)))
+                self._utc_offsets = {row[1]: row[2] for row in result}
             except pymssql.Error as ex:
                 LOG.warning('SQL error: ' + str(ex))
             except pymssql.Warning as ex:
@@ -226,32 +250,32 @@ class PredictionPoller:
     def _gather_predictions_per_stop(self, result):
         predictions_by_stop = {}
         # FIXME: Check for initialization only once in a cleaner way.
-        if (self._stops is not None and self._lines is not None and
+        if (self._stops is not None and self._journeys is not None and
                 self._utc_offsets is not None):
             for row in result:
-                dvj = row[1]
-                line_info = self._lines.get(dvj, None)
-                if line_info is None:
-                    LOG.warning('This DatedVehicleJourneyId was not found '
-                                'from collected line information: ' + dvj)
+                dvj = row[2]
+                journey_info = self._journeys.get(dvj, None)
+                if journey_info is None:
+                    LOG.warning('This DatedVehicleJourneyUniqueGid was not '
+                                'found from collected journey information: ' +
+                                dvj +  '. Prediction row was: ' + str(row))
                     continue
                 utc_offset = self._utc_offsets.get(dvj, None)
                 if utc_offset is None:
-                    LOG.warning('This DatedVehicleJourneyId was not found '
-                                'from collected UTC offset information: ' +
-                                dvj)
+                    LOG.warning('This DatedVehicleJourneyUniqueGid was not '
+                                'found from collected UTC offset '
+                                'information: ' + dvj + '. Prediction row '
+                                'was: ' + str(row))
                     continue
-                jpp = row[2]
+                jpp = row[3]
                 stop = self._stops.get(jpp, None)
                 if stop is None:
                     LOG.warning('This JourneyPatternPointGid was not found '
                                 'from collected stop information: ' + jpp)
                     continue
-                # FIXME: Finish this
-                # Use strings from sql server, add hours in the end + 03:00
-                start_naive = line_info['LocalizedStartTime']
-                scheduled_naive = row[3]
-                predicted_naive = row[4]
+                start_naive = journey_info['LocalizedStartTime']
+                scheduled_naive = row[4]
+                predicted_naive = row[5]
                 start_time = _combine_into_timestamp(start_naive, utc_offset)
                 scheduled_time = _combine_into_timestamp(scheduled_naive,
                                                          utc_offset)
@@ -259,8 +283,8 @@ class PredictionPoller:
                                                          utc_offset)
                 prediction = {
                     'joreStopId': stop,
-                    'joreLineId': line_info['JoreLineId'],
-                    'joreLineDirection': line_info['JoreDirection'],
+                    'joreLineId': journey_info['JoreLineId'],
+                    'joreLineDirection': journey_info['JoreDirection'],
                     'journeyStartTime': start_time,
                     'scheduledArrivalTime': scheduled_time,
                     'predictedArrivalTime': predicted_time,
@@ -276,22 +300,27 @@ class PredictionPoller:
     async def _keep_polling_predictions(self):
         PREDICTION_QUERY = """
             SELECT
-                CONVERT(CHAR(16), Arrival.Id) AS ArrivalId,
-                CONVERT(CHAR(16), DatedVehicleJourney.Id)
-                AS DatedVehicleJourneyId,
-                CONVERT(CHAR(16), Arrival.IsTargetedAtJourneyPatternPointGid)
-                AS JourneyPatternPointGid,
-                Arrival.TimetabledLatestDateTime,
-                Arrival.EstimatedDateTime,
-                Arrival.LastModifiedUTCDateTime
+                CONVERT(CHAR(16), A.Id) AS ArrivalId,
+                CONVERT(CHAR(16), DVJ.Id
+                ) AS DatedVehicleJourneyId,
+                CONCAT(
+                    CONVERT(CHAR(8), DVJ.OperatingDayDate, 112),
+                    ':',
+                    CONVERT(CHAR(16), DVJ.Gid)
+                ) AS DatedVehicleJourneyUniqueGid,
+                CONVERT(CHAR(16), A.IsTargetedAtJourneyPatternPointGid
+                ) AS JourneyPatternPointGid,
+                A.TimetabledLatestDateTime,
+                A.EstimatedDateTime,
+                A.LastModifiedUTCDateTime
             FROM
-                Arrival,
-                DatedVehicleJourney
+                Arrival AS A,
+                DatedVehicleJourney AS DVJ
             WHERE
-                Arrival.IsOnDatedVehicleJourneyId = DatedVehicleJourney.Id
-                AND Arrival.LastModifiedUTCDateTime >= '{modified_utc}'
-                AND Arrival.EstimatedDateTime IS NOT NULL
-                AND Arrival.LastModifiedUTCDateTime IS NOT NULL
+                A.IsOnDatedVehicleJourneyId = DVJ.Id
+                AND A.LastModifiedUTCDateTime >= '{modified_utc}'
+                AND A.EstimatedDateTime IS NOT NULL
+                AND A.LastModifiedUTCDateTime IS NOT NULL
         """
         now = datetime.datetime.utcnow()
         modified_utc_dt = (now - datetime.timedelta(
@@ -301,11 +330,16 @@ class PredictionPoller:
                             modified_utc_dt.strftime('%f')[:3])
             query = PREDICTION_QUERY.format(modified_utc=modified_utc)
             await self._async_helper.wait_for_event(self._is_mqtt_connected)
+            LOG.debug('Querying predictions from ROI:' + query)
+            handled_arrival_ids = set()
             try:
                 result = await self._async_helper.run_in_executor(
                     _connect_and_query, self._roi_connect, query)
-                message_timestamp = isodate.datetime_isoformat(
-                    datetime.datetime.utcnow())
+                pre_len = len(result)
+                # FIXME: bug in here, nothing is excluded
+                result = [row for row in result if not (row[6] == modified_utc_dt and row[0] in handled_arrival_ids)]
+                LOG.debug('Got {pre_len} predictions of which {post_len} were new.'.format(pre_len=pre_len, post_len=len(result)))
+                message_timestamp = _combine_into_timestamp(datetime.datetime.utcnow(), 0)
                 # We will get the latest predictions again next time but it is
                 # more important not to miss any predictions than to not repeat
                 # predictions.
@@ -313,7 +347,12 @@ class PredictionPoller:
                 # Note that at least on 2016-09-13 the Microsoft SQL Server
                 # datetime data type has less than a millisecond precision due
                 # to rounding.
-                modified_utc_dt = max(row[5] for row in result)
+                modified_utc_dt = max(row[6] for row in result)
+                LOG.debug('modified_utc_dt is ' + str(modified_utc_dt))
+                LOG.debug('minimum modified_utc_dt is ' + str(min(row[6] for row in result)))
+                handled_arrival_ids = set(row[0] for row in result if row[6] == modified_utc_dt)
+                LOG.debug('handled_arrival_ids is ' + str(handled_arrival_ids))
+
                 predictions_by_stop = self._gather_predictions_per_stop(result)
                 for stop_id, predictions in predictions_by_stop.items():
                     topic_suffix = stop_id
@@ -334,7 +373,7 @@ class PredictionPoller:
         LOG.debug('PredictionPoller runs.')
         tasks = [
             self._keep_polling_stops(),
-            self._keep_polling_lines(),
+            self._keep_polling_journeys(),
             self._keep_polling_utc_offsets(),
             self._keep_polling_predictions(),
         ]
